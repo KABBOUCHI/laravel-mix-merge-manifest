0019c8ef7d56f7b3a36a6d77c53a10cc9a883a66
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2018-04-05 21:04:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let mix = require('laravel-mix');
+let ManifestPlugin = require('laravel-mix/src/webpackPlugins/ManifestPlugin');
+let merge = require('lodash').merge;
+
+mix.extend('mergeManifest', (config) => {
+    config.plugins.forEach((plugin, index) => {
+        if (plugin instanceof ManifestPlugin) {
+            config.plugins.splice(index, 1);
+        }
+    });
+
+    config.plugins.push(new class {
+        apply(compiler) {
+
+            compiler.plugin('emit', (curCompiler, callback) => {
+                let stats = curCompiler.getStats().toJson();
+
+                Mix.manifest.manifest = merge(Mix.manifest.read(), Mix.manifest.manifest);
+                Mix.manifest.transform(stats).refresh();
+                callback();
+            });
+        }
+    })
+});
