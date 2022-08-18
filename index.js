b70c5e58477f1d5d4ef00f31ee13f2c3afe6d395
Viktor Mazur <vmazu@softserveinc.com>
2022-08-18 19:37:28 +0300

    Add support name file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 let mix = require('laravel-mix');
 let ManifestPlugin = require('laravel-mix/src/webpackPlugins/ManifestPlugin');
+const Manifest = require('laravel-mix/src/Manifest');
 let merge = require('lodash').merge;
 
-mix.extend('mergeManifest', (config) => {
+mix.extend('mergeManifest', (config, nameFile) => {
     config.plugins.forEach((plugin, index) => {
         if (plugin instanceof ManifestPlugin) {
             config.plugins.splice(index, 1);
@@ -13,8 +14,11 @@ mix.extend('mergeManifest', (config) => {
     config.plugins.push(new class {
         apply(compiler) {
 
-            compiler.hooks.emit.tapAsync('ManifestPlugin', (curCompiler, callback) => {
+            compiler.plugin('emit', (curCompiler, callback) => {
                 let stats = curCompiler.getStats().toJson();
+                if (nameFile !== undefined) {
+                    Mix.manifest = new Manifest(`${nameFile}.json`);
+                }
 
                 try {
                     Mix.manifest.manifest = merge(Mix.manifest.read(), Mix.manifest.manifest);
